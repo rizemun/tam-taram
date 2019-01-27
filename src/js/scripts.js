3094bcdf799b2e42763e1eb51b1317cf22203c34
rizemun <rizeman01@gmail.com>
2019-01-27 19:37:27 +0300

    2nd

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,1 +1,21 @@
-// Your code here...
+/*$(document).ready(function () {
+        let params = {
+            'id': 'KD7A16-v9b0',
+            "part": 'snippet',
+            "type": 'video',
+            "key": 'AIzaSyDiaw_MaLTdUJ1d3M64ClGetDlLQWC8B-U'
+        };
+        let url = 'https://www.googleapis.com/youtube/v3/search';
+        $.getJSON(url,params,function(c){
+            c.items.forEach(item => {
+                console.log(item.snippet.thumbnails.default.url)
+            });
+
+
+
+        })
+
+
+    }
+);
+*/
