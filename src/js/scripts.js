697756f5230a8da1ba0059307ef4775d21ccb716
rizemun <rizeman01@gmail.com>
2019-01-31 21:38:29 +0300

    Add Slink buttons to scroll.

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -20,4 +20,36 @@
 );
 */
 
-/*
+
+
+
+$(document).ready(function() {
+
+    $('.videos__content').slick({
+        infinite: false,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: true,
+        nextArrow: '<svg class="videos__button videos__button_next"><use xlink:href="img/svg/ico_dropdown.svg#Layer_1"></svg>',
+        prevArrow: '<svg class="videos__button videos__button_prev videos__button_inactive"><use xlink:href="img/svg/ico_dropdown.svg#Layer_1"></svg>',
+
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+
+});
