c425b8b3967aec3c2f3f64ec8704597e860c8be1
rizemun <rizeman01@gmail.com>
2019-02-04 03:21:30 +0300

    Fix stages betwwn mobile and tablet

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -21,55 +21,58 @@
 */
 
 
+$(document).ready(function () {
+    let prevArrow = '<svg class="videos__button videos__button_next"><use xlink:href="img/svg/ico_dropdown.svg#Layer_1"></svg>';
+    let nextArrow = '<svg class="videos__button videos__button_prev"><use xlink:href="img/svg/ico_dropdown.svg#Layer_1"></svg>';
 
+    if (screen.width >= 768) {
 
-$(document).ready(function() {
+        $('.videos__slick').slick({
 
-    $('.videos__slick').slick({
-        infinite: false,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        vertical: false,
-        //mobileFirst: true,
-        arrows: true,
-        nextArrow: '<svg class="videos__button videos__button_next"><use xlink:href="img/svg/ico_dropdown.svg#Layer_1"></svg>',
-        prevArrow: '<svg class="videos__button videos__button_prev"><use xlink:href="img/svg/ico_dropdown.svg#Layer_1"></svg>',
+            vertical: false,
+            arrows: true,
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            nextArrow: prevArrow,
+            prevArrow: nextArrow,
 
-        responsive: [
-            {
-                breakpoint: 992,
-                settings: {
-                    vertical: false,
-                    slidesToShow: 2,
-                    slidesToScroll: 1,
-                    infinite: false
+            responsive: [
+                {
+                    breakpoint: 992,
+                    settings: {
+                        vertical: false,
+                        slidesToShow: 2,
+                        slidesToScroll: 1,
+                        infinite: false
+                    }
+                },
+                {
+                    breakpoint: 768,
+                    settings: {
+                        infinite: false,
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                        arrows: false,
+                    }
+                },
+                {
+                    breakpoint: 390,
+                    settings: "unslick"
                 }
-            },
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1
-                }
-            },
-            {
-                breakpoint: 390,
-                settings: "unslick"
-            }
-        ]
-    });
+            ]
+        });
+    }
 
-
-    $('.videos__more').on('click', function(e){
+    $('.videos__more').on('click', function (e) {
         this.style.height = 0;
         const className = "videos__mobile-hidden";
         const parent = $(this).parent();
         const videos = parent.children()[1].children;
 
 
-        [].forEach.call(videos, function(video){
+        [].forEach.call(videos, function (video) {
             const $video = $(video);
-            if ($video.hasClass(className)){
+            if ($video.hasClass(className)) {
                 $video.removeClass(className);
             }
         });
